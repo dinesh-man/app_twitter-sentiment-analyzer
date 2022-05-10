d697d815aa5a82fc77c5a83eaff0958251b74ac9
Dinesh Mandal <dinesh.mandal@xandr.com>
2022-05-11 00:05:30 +0530

    Created initial version for POC

---
+++ auth/connectTwitter.go
@@ -0,0 +1,34 @@
+package connectTwitter
+
+import (
+"github.com/dghubble/go-twitter/twitter"
+"github.com/dghubble/oauth1"
+)
+
+type Credentials struct {
+    ApiKey              string
+    ApikeySecret        string
+    AccessToken         string
+    AccessTokenSecret   string
+}
+
+func GetClient(creds *Credentials) (*twitter.Client, error){
+	api := oauth1.NewConfig(creds.ApiKey, creds.ApikeySecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+	http_client := api.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(http_client)
+
+	verifyParams := &twitter.AccountVerifyParams{
+        SkipStatus:   twitter.Bool(true),
+        IncludeEmail: twitter.Bool(true),
+    }
+
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+    if err != nil {
+        fmt.Println("Error getting Twitter!!")
+		return nil, err
+    }else{
+	fmt.Printf("Authentication Successfull.\nUser Name: %+v\nTwitter Handle: %+v", user.Name, user.ScreenName)
+	return client, nil}
+}
+
